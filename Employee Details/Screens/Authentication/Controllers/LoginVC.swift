202565200cb57eff620c953835d842ec39744d35
Sooraj R <sooraj.r.dev@gmail.com>
2024-10-24 10:28:18 +0530

    Initial Commit (project created)

---
+++ Employee Details/Screens/Authentication/Controllers/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  Employee Details
+//
+//  Created by Sooraj R on 24/10/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
